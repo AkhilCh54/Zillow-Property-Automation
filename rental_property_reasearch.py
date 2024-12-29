fbbc0efefe7d11e9c125c5eef83ffca3f0e36f14
AkhilCh54 <akhilcherukuri54@gmail.com>
2024-12-29 15:31:05 -0600

    Add files via upload

--- rental_property_reasearch.py
+++ rental_property_reasearch.py
@@ -11,7 +11,7 @@ headers = {
     "Accept-Language":"en-US,en;q=0.9",
 }
 
-zillow_url = "https://www.zillow.com/arlington-tx/rentals/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3A%22Arlington%2C%20TX%22%2C%22mapBounds%22%3A%7B%22west%22%3A-97.3171655595703%2C%22east%22%3A-96.95324344042967%2C%22south%22%3A32.55329755890131%2C%22north%22%3A32.85029025582152%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A50763%2C%22regionType%22%3A6%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22min%22%3A0%2C%22max%22%3A388188%7D%2C%22mp%22%3A%7B%22min%22%3A0%2C%22max%22%3A2000%7D%2C%22beds%22%3A%7B%22min%22%3A2%7D%2C%22ah%22%3A%7B%22value%22%3Atrue%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22baths%22%3A%7B%22min%22%3A1.5%7D%2C%22sf%22%3A%7B%22value%22%3Afalse%7D%2C%22tow%22%3A%7B%22value%22%3Afalse%7D%2C%22rad%22%3A%7B%22value%22%3A%222023-08-26%22%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A11%7D"
+zillow_url = "" #Your zillow area URL
 response = requests.get(url=zillow_url,headers=headers)
 data = response.text
 soup = BeautifulSoup(data, "lxml")
@@ -38,7 +38,7 @@ count=0
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(chrome_options)
-driver.get("https://docs.google.com/forms/d/e/1FAIpQLSeAiJqzApAv2Z4D_ryGjhdpKwxHMiXJza5ONXQFkTouVCPAvA/viewform")
+driver.get("") #Your google form URL
 def submitting_google_form():
     global count
     time.sleep(2)
